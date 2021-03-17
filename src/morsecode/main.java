bd9af91ca37cea0fb924c55159e10434e3b33582
Lukas Langrock <lukas.langrock@mailbox.org>
2021-03-17 08:48:57 +0100

    Add menu to morse main

--- src/morsecode/main.java
+++ src/morsecode/main.java
@@ -7,11 +7,22 @@ public class main {
      * Main method for running strings against the lalelu-langauge.
      */
     public static void main(String[] args) {
-        String input = TIO.AnfrageString("Enter input to translate: ");
+        String reverseMode = TIO.AskString("Do you wish to translate an input to morse [n] vor in reverse [y]?");
 
-        Worker worker = new Worker();
-        String output = worker.translateToChar(input);
+        if (reverseMode.equals("n")) {
+            String input = TIO.AskString("Enter input to translate: ");
 
-        TIO.prt("Translated output: '" + output + "'");
+            Worker worker = new Worker();
+            String output = worker.translateToChar(input);
+    
+            TIO.prt("Translated output: '" + output + "'");
+        } else if (reverseMode.equals("y")) {
+            String input = TIO.AskString("Enter input to translate: ");
+
+            Worker worker = new Worker();
+            String output = worker.translateStringToMorse(input);
+    
+            TIO.prt("Translated output: '" + output + "'");
+        }
     }
 }
