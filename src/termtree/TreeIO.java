05b6caadd7724a68d29b9a57a34000c1f01c7be3
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-14 09:25:21 +0200

    Add TreeIO

---
+++ src/termtree/TreeIO.java
@@ -0,0 +1,17 @@
+package termtree;
+
+import lib.BinaryTree;
+
+public class TreeIO {
+    public BinaryTree<Termchar> treeFromPrefix(String pInput) {
+        return null;
+    }
+
+    public BinaryTree<Termchar> treeFromInfix(String pInput) {
+        return null;
+    }
+
+    public BinaryTree<Termchar> treeFromPostfix(String pInput) {
+        return null;
+    }
+}
