dc39d943c3df2a7c8f6edf775fe3f4763ecb1787
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-19 15:10:17 +0200

    [termtree] Optimize and Add main logic

--- src/termtree/Termchar.java
+++ src/termtree/Termchar.java
@@ -1,14 +1,15 @@
 package termtree;
 
 public class Termchar {
+    private static final String OperatorPool = "+-/*";
+
     private char content;
     private final boolean isOperator;
     private final boolean isNumber;
-    private final String operatorPool = "+-/*";
 
     public Termchar(char pInput) {
         content = pInput;
-        if (operatorPool.indexOf(pInput) != -1) { // check if input char is an operator by checking if the char is inside the pool of operator chars
+        if (OperatorPool.indexOf(pInput) != -1) { // check if input char is an operator by checking if the char is inside the pool of operator chars
             isOperator = true;
         } else { isOperator = false; }
         if (Character.isDigit(pInput)) { // check if input is a number
--- src/termtree/main.java
+++ src/termtree/main.java
@@ -1,9 +1,83 @@
 package termtree;
 
+import org.graalvm.compiler.lir.amd64.vector.AMD64VectorMove.RestoreRegistersOp;
+
 import lib.TIO;
 
 public class main {
     public static void main(String[] args) {
-        // todo
+        boolean allowMainExit = true; // set to false to restart termtree main
+        boolean restartLoop = true; // set to true to automatically restart the loop without asking
+        boolean forceExitLoop = false; // set to true to automatically exit the loop without asking, overrides 
+
+        do {
+            Termtree termtree = new Termtree();
+            int importFormat = TIO.AskInt("Import a tree using prefix[1], infix[2], or postfix[3]: ");
+            String importString = TIO.AskString("Paste your tree: ");
+
+            switch (importFormat) {
+            case 1:
+                termtree.importFromPrefix(importString);
+                TIO.prt("Tree was imported from Prefix!\n");
+                break;
+
+            case 2:
+                termtree.importFromInfix(importString);
+                TIO.prt("Tree was imported from Infix!\n");
+                break;
+
+            case 3:
+                termtree.importFromPostfix(importString);
+                TIO.prt("Tree was imported from Postfix!\n");
+                break;
+
+            default:
+                TIO.prt("Invalid importing option! Please select 1, 2, or 3.");
+                restartLoop = false;
+                allowMainExit = false;
+                break;
+            }
+
+            
+            while (restartLoop) {
+                restartLoop = false; // makes the user select to restart/cancel
+                int exportFormat = TIO.AskInt("Export the tree using prefix[1], infix[2] or postfix[3] or overwrite[4] it with a different tree: ");
+
+                switch (exportFormat) {
+                case 1:
+                    termtree.importFromPrefix(importString);
+                    TIO.prt("Tree was imported from Prefix!\n");
+                    break;
+
+                case 2:
+                    termtree.importFromInfix(importString);
+                    TIO.prt("Tree was imported from Infix!\n");
+                    break;
+
+                case 3:
+                    termtree.importFromPostfix(importString);
+                    TIO.prt("Tree was imported from Postfix!\n");
+                    break;
+
+                case 4:
+                    forceExitLoop = true;
+                    allowMainExit = false;
+                    break;
+
+                default:
+                    TIO.prt("Invalid exporting option! Please select 1, 2, 3, or 4.");
+                    restartLoop = true;
+                    break;
+                }
+
+                // exit or restart logic
+                if (forceExitLoop) {
+                    restartLoop = false; // forceExitLoop overrides restartLoop = true
+                }
+                if (!restartLoop && TIO.AskString("Do you wish to continue? [y/N]: ") == "y") { // ask user to rstart/cancel if forceExit = false && cancelExit = false 
+                    restartLoop = true; // do not restart loop upon user request
+                }
+            }
+        } while (!allowMainExit);
     }
 }
