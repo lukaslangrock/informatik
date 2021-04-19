19a254401bf856714b85fea361fd24ddac8fcd3d
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-19 10:28:35 +0200

    [termtree] Update Termchar.java

--- src/termtree/Termchar.java
+++ src/termtree/Termchar.java
@@ -2,13 +2,21 @@ package termtree;
 
 public class Termchar {
     private char content;
-    private boolean isOperator;
+    private final boolean isOperator;
+    private final boolean isNumber;
+    private final String operatorPool = "+-/*";
 
-    public Termchar(char pInput, boolean pIsOperator) {
+    public Termchar(char pInput) {
         content = pInput;
-        isOperator = pIsOperator;
+        if (operatorPool.indexOf(pInput) != -1) { // check if input char is an operator by checking if the char is inside the pool of operator chars
+            isOperator = true;
+        } else { isOperator = false; }
+        if (Character.isDigit(pInput)) { // check if input is a number
+            isNumber = true;
+        } else { isNumber = false; }
     }
 
     public char getContent() { return content; }
     public boolean getIsOperator() { return isOperator; }
+    public boolean getIsNumber() { return isNumber; }
 }
