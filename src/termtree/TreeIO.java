43f8a29e1b1f52e362a72d57183fb78f0c3b122f
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-19 11:16:49 +0200

    [termtree] Save progress

--- src/termtree/TreeIO.java
+++ src/termtree/TreeIO.java
@@ -3,8 +3,30 @@ package termtree;
 import lib.BinaryTree;
 
 public class TreeIO {
+    private static Termchar[] subTermtreeArray(Termchar[] pInput) {
+        Termchar[] output = new Termchar[pInput.length -1];
+        for (int i = 1; i < pInput.length - 1; i++) {
+            output[i -1] = pInput[i];
+        }
+        return output;
+    }
+
     public static BinaryTree<Termchar> treeFromPrefix(Termchar[] pInput) {
-        return null;
+        BinaryTree<Termchar> tree = new BinaryTree<Termchar>();
+        if (pInput == null || Character.isWhitespace(pInput[0].getContent())) {
+            return null;
+        }
+
+        tree.setContent(pInput[0]);
+        if (pInput[0].getIsOperator()) {
+            tree.setLeftTree(treeFromPrefix(subTermtreeArray(pInput)));
+            tree.setRightTree(treeFromPrefix(subTermtreeArray(pInput)));
+
+            // rekursion: left tree mit pInput[] -1 aktueller i
+            // rekursion same but right
+        }
+
+        return tree;
     }
 
     public static BinaryTree<Termchar> treeFromInfix(Termchar[] pInput) {
