83dcfc259f39d2a532e8fcde65d5ccb1b4e2da31
Lukas Langrock <lukas.langrock@mailbox.org>
2021-05-03 15:16:08 +0200

    [chartree] fix ;

--- src/chartree/main.java
+++ src/chartree/main.java
@@ -6,7 +6,7 @@ public class main {
 
     public static void main(String[] args) {
         CharController controller = null;
-        boolean initialized = false;;
+        boolean initialized = false;
 
         boolean repeat = true;
         do {
