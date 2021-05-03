045c01844eb64dce418138006e58f31089999651
Lukas Langrock <lukas.langrock@mailbox.org>
2021-05-03 14:15:28 +0200

    [chartree] Add CharController logic

--- src/chartree/CharController.java
+++ src/chartree/CharController.java
@@ -1,5 +1,49 @@
 package chartree;
 
+import org.graalvm.compiler.graph.spi.Canonicalizable.Binary;
+
+import lib.BinarySearchTree;
+
 public class CharController {
-    // todo
+    private BinarySearchTree<CharItem> tree;
+    private String word;
+
+    public CharController(String pInput) {
+        tree = new BinarySearchTree<CharItem>();
+        word = pInput;
+
+        for (Character i : pInput.toCharArray()) {
+            sortCharInTree(i);
+        }
+    }
+
+    private void sortCharInTree(Character pInput) {
+        CharItem item = new CharItem(pInput);
+        CharItem existingItem = tree.search(item);
+
+        if (existingItem == null) {
+            tree.insert(item);
+        } else {
+            existingItem.incrementCount();
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public BinarySearchTree<CharItem> getCharSearchTree() {
+        return tree;
+    }
+
+    public int getCount(Character pInput) {
+        CharItem item = new CharItem(pInput);
+        CharItem existingItem = tree.search(item);
+
+        if (existingItem == null) {
+            return 0;
+        } else {
+            return existingItem.getCount();
+        }
+    }
 }
